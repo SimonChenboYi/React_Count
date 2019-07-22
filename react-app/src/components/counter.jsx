56116127068cadd1fe3c9692ce5a7db6ec9e5605
Chenbo Yi <yichenbo@outlook.com>
2019-07-22 17:05:01 +0100

    render list

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -2,18 +2,27 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    imageUrl: 'https://picsum.photos/200'
+    count: 1,
+    tags: ['tag1', 'tag2', 'tag3']
   };
 
   render() {
+
     return (
-      <React.Fragment>
-        <img src={this.state.imageUrl} alt="" />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span style={{fontSize:50}} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button style={{fontSize:40}} className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag =><li key={tag}>{tag}</li>)}
+        </ul>
+      </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
 formatCount(){
