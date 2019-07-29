df4a3c9776ad6162b97a4a0d445a86a7a0ff7fc6
Chenbo Yi <yichenbo@outlook.com>
2019-07-29 10:35:44 +0100

    set the counter value by props

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: ["tag1", "tag2", "tag3"]
   };
 
   handleIncrement= () => {
-    this.setState({ count: this.state.count + 1 })
+    this.setState({ value: this.state.value + 1 })
   }
 
   renderTags() {
@@ -23,7 +23,7 @@ class Counter extends Component {
   }
 
   handleIncrement = ()=> {
-    this.setState({ count: this.state.count+1 });
+    this.setState({ value: this.state.value+1 });
   };
 
   render() {
@@ -49,13 +49,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : count;
+    const { value } = this.state;
+    return value === 0 ? <h1>Zero</h1> : value;
   }
 }
 
